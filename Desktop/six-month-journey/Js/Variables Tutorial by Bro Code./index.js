cb40f6ce5408adbb707fcc3476648c9167ff4299
codesaveslives <sirelexzy@gmail.com>
2025-08-13 21:59:50 +0100

    Add first mini JavaScript project

---
+++ Desktop/six-month-journey/Js/Variables Tutorial by Bro Code./index.js
@@ -0,0 +1,11 @@
+let fullName = "Endangered Coder";
+let age = 25;
+let student = true;
+
+console.log(student);
+
+document.getElementById("p1").textContent = `Your name is ${fullName}.`;
+
+document.getElementById("p2").textContent = `You are ${age} years old.`;
+
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
